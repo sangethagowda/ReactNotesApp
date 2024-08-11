3eb55d2b346afdf09c14d50f374bf14825b34692
Sangeetha <sangeethaGowda@gmail.com>
2024-08-11 23:01:23 +0530

    added notes

--- notes-app-ui/src/App.tsx
+++ notes-app-ui/src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { title } from 'process';
+import { mockNoteData } from './Constants/constant';
+import { Note } from './interface/projectInterface';
 
 function App() {
+  const [notes,setNotes] =useState<Note[]>(mockNoteData);
+  const [title,setTitle] = useState('');
+  const [content,setContent] = useState('');
+  const handleSubmit=(event:React.FormEvent)=>{
+    event.preventDefault();
+    const newNote:Note={
+      id: notes.length,
+      title: title,
+      content: content
+    };
+    setNotes([newNote,...notes]);
+    setTitle('');
+    setContent('');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+        <form className='note-form' onSubmit={(event)=>handleSubmit(event)}>
+          <input placeholder='title'
+            value={title}
+            onChange={(event)=>{
+              setTitle(event.target.value)
+            }}
+           required></input>
+          <textarea 
+          placeholder='content' 
+          value={content}
+          onChange={(event)=>{
+            setContent(event.target.value)
+          }}
+          required rows={10}></textarea>
+          <button type='submit'>Add Note</button>
+        </form>
+        
+        <div className='notes-grid'>
+        {
+          notes.map((note:Note)=>{
+            return(<div className='notes-item'>
+              <div className='notes-header'>
+                <button>
+                  X
+                </button>
+              </div>
+              <h2>{note.title}</h2>
+              <p>{note.content}</p>
+            </div>)
+          })
+        }
+        </div>
     </div>
   );
 }
---
+++ notes-app-ui/src/Constants/constant.ts
@@ -0,0 +1,20 @@
+export const mockNoteData=[{
+    id:1,
+    title:"note title 1",
+    content:"content 1"
+  },
+  {
+    id:2,
+    title:"note title 2",
+    content:"content 2"
+  },
+  {
+    id:3,
+    title:"note title 3",
+    content:"content 3"
+  },
+  {
+    id:4,
+    title:"note title 4",
+    content:"content 4"
+  }]
